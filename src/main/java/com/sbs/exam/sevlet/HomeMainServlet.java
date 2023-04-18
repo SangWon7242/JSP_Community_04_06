7c324ed1364df56c225fffeb83f883aa560aa148
SangWon7242 <gotkdjqa@gmail.com>
2023-04-18 20:36:44 +0900

    로그아웃 기능 구현

--- src/main/java/com/sbs/exam/sevlet/HomeMainServlet.java
+++ src/main/java/com/sbs/exam/sevlet/HomeMainServlet.java
@@ -6,6 +6,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
@@ -14,9 +15,17 @@ public class HomeMainServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     Rq rq = new Rq(req, resp);
+    HttpSession session = req.getSession();
 
-    req.setAttribute("age", 22);
-    req.setAttribute("isLogined", false);
+    boolean isLogined = false;
+    int loginedMemberId = -1;
+    if(session.getAttribute("loginedMemberId") != null) {
+      loginedMemberId = (int) session.getAttribute("loginedMemberId");
+      isLogined = true;
+    }
+
+    req.setAttribute("isLogined", isLogined);
+    req.setAttribute("loginedMemberId", loginedMemberId);
 
     rq.jsp("../home/main");
   }
--- src/webapp/home/main.jsp
+++ src/webapp/home/main.jsp
@@ -1,8 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 
 <%
-  int age = (int) request.getAttribute("age");
   boolean isLogined = (boolean) request.getAttribute("isLogined");
+  int loginedMemberId = (int) request.getAttribute("loginedMemberId");
 %>
 
 <!doctype html>
@@ -13,16 +13,22 @@
 <body>
   <h1>메인 페이지</h1>
 
-  <% if( isLogined ) { %>
-    <div>나이는 <%=age%>살 입니다.</div>
-  <% } %>
-
   <div>
     <a href="../article/list">게시물 리스트</a>
     &nbsp;
     <a href="../member/join">회원가입</a>
-    &nbsp;
-    <a href="../member/login">로그인</a>
+    
+    <% if( !isLogined ) { %>
+      &nbsp;
+      <a href="../member/login">로그인</a>
+    <% } %>
+
+    <% if( isLogined ) { %>
+      &nbsp;
+      <%=loginedMemberId%>번 회원님 환영합니다.
+      <a href="../member/doLogout">로그아웃</a>
+    <% } %>
+
   </div>
 
 </body>
