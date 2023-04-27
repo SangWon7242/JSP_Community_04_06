1961b8925497d02c478c8e2d4fd8897acdd20bcc
SangWon7242 <gotkdjqa@gmail.com>
2023-04-27 19:33:08 +0900

    세션 도입 이유 설명, 로그인 상태시에 로그아웃 버튼이 보여지도록

--- src/main/java/com/sbs/exam/dto/Member.java
+++ src/main/java/com/sbs/exam/dto/Member.java
@@ -13,6 +13,7 @@ public class Member {
   public String updateDate;
   public String loginId;
   public String loginPw;
+  public String name;
 
   public Member(Map<String, Object> row) {
     this.id = (int) row.get("id");
@@ -20,5 +21,6 @@ public class Member {
     this.updateDate = (String) row.get("updateDate");
     this.loginId = (String) row.get("loginId");
     this.loginPw = (String) row.get("loginPw");
+    this.name = (String) row.get("name");
   }
 }
